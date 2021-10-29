cea007d724889348bc0f3569cca87e05af4fad3d
Jakub Ładkowski <JakLad@Pracownia.com>
2021-10-29 14:42:47 +0200

    komentarz

---
+++ Egzamin Komis/auto.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <link rel="stylesheet" href="auto.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Komis Samochodowy</title>
+</head>
+<body>
+    <div id='baner'>
+        <h1>SAMOCHODY</h1>
+</div>
+
+<div class='panellewy'>
+    <h2>Wykaz Samochodów</h2>
+    <ul>
+        <li>1 Fiat Punto</li>
+        <li>2 Fiat Punto</li>
+        <li>3 Fiat Punto</li>
+        <li>4 Opel Corsa</li>
+        <li>5 Opel Astra</li>
+        <li>6 Toyota Corolla</li>
+        <li>7 Toyota Corolla</li>
+        <li>8 Toyota Yaris</li>
+    </ul>
+    <h2>Zamówienia</h2>
+    <ul>
+        <li>3 Anna Kowalska<li>
+        <li>6 Jan Nowakowski</li>
+        <li>8 Marcin Kolwal</li>
+    </ul>
+</div>
+ 
+<div class='panelprawy'>
+    <h2>Pełne dane Fiat</h2>
+</div>
+
+<div class='stopka'>
+<table>
+    <tr>
+        <td>
+            <a target="blank" href="Kwerendy.txt">kwerendy</a>
+        </td>
+        <td>
+            Autor: 9847328523
+        </td>
+
+        <td>
+        <img src="auto.png" alt="komis samochodowy">
+        </td>
+    </tr>
+</table>
+</div>
+
+</body>
+</html>
